bdbb15e72770ccf6d047511d419e267ac669b97e
Christopher Collins <crcollins@cmu.edu>
2019-02-13 17:02:13 -0500

    Add a sanity check to the tests

--- run_all_tests.sh
+++ run_all_tests.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-clear & clear
+if [[ "$1" == "sanity" ]];
+then
+    # The ! is to invert the status code for the whole line
+    ! python -c "import dragonfly" | grep -q "No module named dragonfly"
+    exit $?
+fi
+
 for f in $(find . -name "unittest*" | grep -v ".pyc" | sed -e 's_\./__;s_/_._g;s/\.py//');
 do
     python -m unittest $f;
