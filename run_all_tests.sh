b6a1cb3180a44a9c40f39e9678984d8d0a9b7c76
Christopher Collins <crcollins@cmu.edu>
2019-02-13 20:52:24 -0500

    Add ability to check all possible install states
    This adds a relatively simple loop and switch to check the install state
    of dragonfly. There are three things that are optionally checked with
    their corresponding key (in parenthesis):
    
    1) Dragonfly can be imported. (sanity)
    2) Direct can be imported. (direct)
    3) Optimal transport can be imported. (optimal)
    
    If any of these are unable to import and the corresponding key is given
    as an argument to the tests, then the tests will fail and print the
    value that failed.
    
    Example:
    
    $ bash run_all_tests.sh sanity
    This checks to make sure that dragonfly can be imported. This will
    return a nonzero exit code if it can not.
    
    $ bash run_all_tests.sh sanity direct
    This will check that dragonfly and direct can be imported.
    
    $ bash run_all_tests.sh sanity optimal
    This will check that dragonfly and optimal transport can be imported.
    
    Note: All of these tests come with the prerequisite that 'sanity' is
    always the first test. This is because the others will not make sense
    outside of that context.

--- run_all_tests.sh
+++ run_all_tests.sh
@@ -2,9 +2,29 @@
 
 if [[ "$1" == "sanity" ]];
 then
-    # The ! is to invert the status code for the whole line
-    ! python -c "import dragonfly" | grep -q "No module named dragonfly"
-    exit $?
+    # Try importing dragonfly and save stdout and stderr
+    res=$(python -c "import dragonfly" 2>&1)
+
+    # Default value to reduce over
+    code=1
+    # Scan over all arguments
+    for o in "$@"
+    do
+        case "$o" in
+        sanity) echo "$res" | grep "No module named dragonfly" ;;
+        direct) echo "$res" | grep "Could not import fortran direct" ;;
+        optimal) echo "$res" | grep "Could not import Python optimal transport" ;;
+        *) echo "The accepted inputs are 'sanity', 'direct', and 'optimal'." ;;
+        esac
+        code=$(( $code & $?))
+    done
+
+    # Return nonzero exit code if any value was found
+    if [[ $code == 0 ]];
+    then
+        exit 1;
+    fi
+    exit 0;
 fi
 
 for f in $(find . -name "unittest*" | grep -v ".pyc" | sed -e 's_\./__;s_/_._g;s/\.py//');
