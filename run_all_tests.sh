0d2a23cd147b08875ebee067d677b15edb2a602e
kirthevasank <kandasamy@cs.cmu.edu>
2019-02-20 12:36:26 -0500

    Fixed issues with travis build fails. Removed POT testing for now.

--- run_all_tests.sh
+++ run_all_tests.sh
@@ -16,9 +16,9 @@ then
     do
         case "$o" in
         sanity) echo "$res" | grep "No module named dragonfly" ;;
-        direct) echo "$res" | grep "Could not import fortran direct" ;;
+        direct) echo "$res" | grep "Could not import Fortran direct" ;;
         optimal) echo "$res" | grep "Could not import Python optimal transport" ;;
-        -direct) echo "$res" | grep -v "Could not import fortran direct" && echo "Has direct" ;;
+        -direct) echo "$res" | grep -v "Could not import Fortran direct" && echo "Has direct" ;;
         -optimal) echo "$res" | grep -v "Could not import Python optimal transport" && echo "Has optimal" ;;
         *) echo "The accepted inputs are 'sanity', 'direct', '-direct', 'optimal' and '-optimal'." ;;
         esac
@@ -33,3 +33,4 @@ for f in $(find . -name "unittest*" | grep -v ".pyc" | sed -e 's_\./__;s_/_._g;s
 do
     python -m unittest $f;
 done
+
