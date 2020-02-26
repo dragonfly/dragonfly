4e585edf98875ea0b291aaf69d739a59074b66fa
Anthony Yu <anthonyhsyu@berkeley.edu>
2020-02-26 15:47:27 -0800

    Fix domain constraint issue

--- dragonfly/apis/api_utils.py
+++ dragonfly/apis/api_utils.py
@@ -178,10 +178,10 @@ def preprocess_multifidelity_arguments(fidel_space, domain, funcs, fidel_cost_fu
        domain.get_type() == 'cartesian_product':
     if fidel_space.num_domains == 1 and \
         fidel_space.list_of_domains[0].get_type() == 'euclidean' and \
-        (not hasattr(fidel_space, 'domain_constraints')) and \
-        fidel_space.domain_constraints is None and \
+        ((not hasattr(fidel_space, 'domain_constraints')) or \
+        fidel_space.domain_constraints is None) and \
         domain.num_domains == 1 and domain.list_of_domains[0].get_type() == 'euclidean' and \
-        (not hasattr(domain, 'domain_constraints')) and domain.domain_constraints is None:
+        ((not hasattr(domain, 'domain_constraints')) or domain.domain_constraints is None):
       # Change the fidelity space
       fidel_space = fidel_space.list_of_domains[0]
       config.fidel_space_orderings.dim_ordering = \
@@ -240,7 +240,7 @@ def preprocess_arguments(domain, funcs, config):
     pass
   elif domain.get_type() == 'cartesian_product':
     if domain.num_domains == 1 and domain.list_of_domains[0].get_type() == 'euclidean' and \
-       (not hasattr(domain, 'domain_constraints')) and domain.domain_constraints is None:
+       ((not hasattr(domain, 'domain_constraints')) or domain.domain_constraints is None):
       domain = domain.list_of_domains[0]
       config.domain_orderings.dim_ordering = config.domain_orderings.dim_ordering[0]
       config.domain_orderings.index_ordering = config.domain_orderings.index_ordering[0]
